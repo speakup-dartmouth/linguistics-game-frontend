6829c1d9cb5b19a0c4acfc604ccd39fad5258884
isabellahoch <40366749+isabellahoch@users.noreply.github.com>
2022-11-15 17:22:45 -0500

    feat: profile doesnt look disgusting anymore

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,10 +1,13 @@
 import {
-  SafeAreaView, Button, Text, TouchableHighlight,
+  SafeAreaView, Text, TouchableHighlight, Image, View,
 } from 'react-native';
 import { logout } from 'redux/slices/authSlice';
 import { useAppDispatch, useAppSelector } from 'redux/hooks';
 import { clearToken } from 'services/storage';
 import { useAppNavigation } from 'navigation/types';
+// import GenericUser from '../assets/generic_user.svg';
+import { ImageAssets } from 'assets/imageAssets';
+import { colors } from 'lib/constants';
 import styles from './styles';
 
 function ProfileScreen(): JSX.Element {
@@ -23,19 +26,41 @@ function ProfileScreen(): JSX.Element {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Welcome, {username}</Text>
+      <Image source={ImageAssets.profileImg} style={styles.profileImg} />
+      <Text style={styles.welcome}>Welcome, {username}</Text>
+      <View style={styles.divider} />
       <TouchableHighlight
-        style={styles.editButton}
+        style={styles.button}
         activeOpacity={0.7}
-        underlayColor="#30aaff"
+        underlayColor={colors.lightBlue}
         onPress={() => navigation.navigate('DemographicsModal')}
       >
-        <Text style={styles.editButtonText}>Edit Demographics</Text>
+        <Text style={styles.buttonText}>Edit Demographics</Text>
       </TouchableHighlight>
-      <Button title="CONSENT" onPress={navigateToConsent} />
-      <Button title="DEMOGRAPHICS" onPress={() => navigation.navigate('DemographicsModal')} />
-      <Button title="CATEGORIES" onPress={() => navigation.navigate('CategoriesModal')} />
-      <Button title="LOG OUT" onPress={signOut} />
+      <TouchableHighlight
+        style={styles.button}
+        activeOpacity={0.7}
+        underlayColor={colors.lightBlue}
+        onPress={navigateToConsent}
+      >
+        <Text style={styles.buttonText}>Adjust Consent</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        style={styles.button}
+        activeOpacity={0.7}
+        underlayColor={colors.lightBlue}
+        onPress={() => navigation.navigate('CategoriesModal')}
+      >
+        <Text style={styles.buttonText}>Update Interests</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        style={styles.button}
+        activeOpacity={0.7}
+        underlayColor={colors.lightBlue}
+        onPress={signOut}
+      >
+        <Text style={styles.buttonText}>Log Out</Text>
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
